f214ca2116e8cf6bd370a5adeadd4c8dd6cda652
Richard Nava <richard.ig.nava@gmail.com>
2021-07-29 11:01:41 -0500

    Initial Commit

---
+++ MovieDecider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDecider
+//
+//  Created by Richard Centeno on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
